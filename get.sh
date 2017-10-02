bf94492404c36fe12cfebaa1a4d1087c719be5c0
Sean Ross-Ross <srossross@tableau.com>
2017-10-02 12:40:48 -0700

    fix get

--- get.sh
+++ get.sh
@@ -2,8 +2,9 @@ set -euo pipefail
 
 VERSTION=${TEMPLATE_VERSION:-v0.0.3-3}
 PREFIX=${TEMPLATE_INSTALL_PREFIX:-"/usr/local/bin"}
+URL_BASE="https://github.com/srossross/template/releases/download"
 
-if [ -z "$TEMPLATE_OS" ]; then
+if [ -z "${TEMPLATE_OS+x}" ]; then
   case "$(uname)" in
     Linux)
       OS="linux"
@@ -19,10 +20,11 @@ else
   OS="$TEMPLATE_OS"
 fi
 
-if [ -z "$TEMPLATE_ARCH" ]; then
+if [ -z "${TEMPLATE_ARCH+x}" ]; then
   case "$(uname -m)" in
     x86_64)
       ARCH="amd64"
+      ;;
     x86)
       ARCH="386"
       ;;
@@ -34,9 +36,9 @@ else
   ARCH="$TEMPLATE_ARCH"
 fi
 
-URL_BASE="https://github.com/srossross/template/releases/download"
+
 echo "Fetching tar: ${URL_BASE}/${VERSTION}/template-$OS-$ARCH.tgz"
-curl --fail ${URL_BASE}/${VERSTION}/template-$OS-$ARCH.tgz -o template-$OS-$ARCH.tgz
+curl --fail -L ${URL_BASE}/${VERSTION}/template-$OS-$ARCH.tgz -o template-$OS-$ARCH.tgz
 
 tar -xvf template-$OS-$ARCH.tgz
 chmod +x ./template-$OS-$ARCH
